c317bb9057e6e499db0d2a23c7e9e96e1b9bd39b
atmiktumane <atmiktumane72@gmail.com>
2024-06-15 17:47:13 +0530

    User Role - SignUp functionality, to send data from frontend to backend and store in database

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,17 +1,61 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
 
 export const Signup = () => {
+  // Input field values
+  const [Values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    address: "",
+  });
+
   // showPassword state
   const [showPassword, setShowPassword] = useState(false);
 
-  // passwordInput state
-  const [passwordInput, setPasswordInput] = useState("");
+  // Navigation
+  const navigate = useNavigate();
 
+  // Update Input Fields
+  const UpdateInputValues = (e) => {
+    const { name, value } = e.target;
+    // console.log(value);
+    setValues({ ...Values, [name]: value });
+  };
+
+  // Show Password Functionality
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  // Submit SignUp data to database through backend api
+  const submitSignupData = async (e) => {
+    e.preventDefault();
+
+    try {
+      // simple validation
+      if (
+        Values.username === "" ||
+        Values.email === "" ||
+        Values.password === "" ||
+        Values.address === ""
+      ) {
+        alert("All Fields are mandatory to fill!");
+        return;
+      }
+
+      const response = await axios.post("/api/v1/register", Values);
+      alert(response.data.message);
+
+      // once user is registered, navigate to login page
+      navigate("/login");
+    } catch (error) {
+      console.error("Error while submitting signUp Data : ", error);
+      alert(error.response.data.message);
+    }
   };
 
   return (
@@ -20,7 +64,10 @@ export const Signup = () => {
       <h3 className="text-zinc-300 text-2xl font-semibold mb-4">Sign Up</h3>
 
       {/* SignUP Form */}
-      <form className="bg-zinc-800 p-6 w-full md:w-3/6 lg:w-2/6 flex flex-col rounded">
+      <form
+        onSubmit={submitSignupData}
+        className="bg-zinc-800 p-6 w-full md:w-3/6 lg:w-2/6 flex flex-col rounded"
+      >
         {/* Username */}
         <label htmlFor="username" className="text-zinc-400 mb-2">
           Username
@@ -30,6 +77,9 @@ export const Signup = () => {
           name="username"
           id="username"
           placeholder="Enter Your Username"
+          required
+          value={Values.username}
+          onChange={UpdateInputValues}
           className="bg-zinc-900 p-2 text-zinc-200 rounded"
         />
 
@@ -42,6 +92,9 @@ export const Signup = () => {
           name="email"
           id="email"
           placeholder="Enter Email Address"
+          required
+          value={Values.email}
+          onChange={UpdateInputValues}
           className="bg-zinc-900 p-2 text-zinc-200 rounded"
         />
 
@@ -57,8 +110,9 @@ export const Signup = () => {
             name="password"
             id="password"
             placeholder="Enter Password"
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
+            required
+            value={Values.password}
+            onChange={UpdateInputValues}
             className="bg-zinc-900 p-2 w-full rounded"
           />
           {/* span tag for showPassword icon */}
@@ -67,7 +121,7 @@ export const Signup = () => {
             onClick={togglePasswordVisibility}
           >
             {/* if password.length > 0 then only show icons else keep it blank */}
-            {passwordInput.length > 0 ? (
+            {Values.password.length > 0 ? (
               // to show icons: check whether to show "showEye icon" or "hideEye icon"
               showPassword ? (
                 <FaEyeSlash />
@@ -89,11 +143,17 @@ export const Signup = () => {
           id="address"
           rows="4"
           placeholder="Enter Your Address"
+          required
+          value={Values.address}
+          onChange={UpdateInputValues}
           className="bg-zinc-900 p-2 text-zinc-200 rounded"
         ></textarea>
 
         {/* SignUp Button */}
-        <button className="mt-4 bg-blue-500 hover:bg-blue-700 px-4 py-1 text-zinc-200 rounded">
+        <button
+          type="submit"
+          className="mt-4 bg-blue-500 hover:bg-blue-700 px-4 py-1 text-zinc-200 rounded"
+        >
           SignUp
         </button>
 
